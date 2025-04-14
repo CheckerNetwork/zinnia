32d3436b563bb44a4d5f89e6d6e5f7d47112da90
Miroslav Bajtoš <oss@bajtos.net>
2025-04-14 15:22:29 +0200

    test: import JSON files
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- runtime/tests/js/module_loader_tests.js
+++ runtime/tests/js/module_loader_tests.js
@@ -16,6 +16,30 @@ test("can import files outside the main module directory", async () => {
   await assertRejects(() => import("../../js/99_main.js"));
 });
 
+test("import JSON files using dynamic `import()` (with attributes)", async () => {
+  const { default: data } = await import("./module_fixtures/data.json", { with: { type: "json" } });
+  assertEquals(data, { name: "Jane Smith", age: 30, email: "jane.smith@example.com" });
+});
+
+test("import JSON files using dynamic `import()` (no attributes)", async () => {
+  const err = await assertRejects(async () => import("./module_fixtures/data.json"));
+  assertMatch(err.message, /"type": "json" attribute/);
+  assertEquals(err.code, "ERR_MODULE_NOT_FOUND");
+});
+
+test("import JSON files using static `import` (with attributes)", async () => {
+  await import("./module_fixtures/import-json-with-attributes.js");
+});
+
+test("import JSON files using static `import` (no attributes)", async () => {
+  const err = await assertRejects(
+    async () => import("./module_fixtures/import-json-no-attributes.js"),
+  );
+
+  assertMatch(err.message, /"type": "json" attribute/);
+  assertEquals(err.code, "ERR_MODULE_NOT_FOUND");
+});
+
 test("cannot import files over http", async () => {
   let err = await assertRejects(() => import("https://deno.land/std@0.181.0/version.ts"));
   assertMatch(err.message, /Zinnia can import local modules only/);
